8fb66a39d4061d1fbdd4ca291ecb91d92ddee696
Ifu Aniemeka <ianiemeka@gmail.com>
2018-04-17 14:23:50 -0500

    Add scrolling effect for landing page navigation

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
   $(".hamburger").on("click", function(e){
-    console.log("justclicked!");
     $("nav").toggleClass("display-mobile-nav");
+  });
+
+  $(".to-about-me, .to-work, .to-contact").click(function() {
+    var $section = $($(this).attr("href"));
+    $('html, body').animate({scrollTop: $section.offset().top}, 'slow');
   });
 });
