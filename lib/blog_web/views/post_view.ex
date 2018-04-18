68507ecca56a23d383c68221cfb7d4d5af9e1c4f
Ifu Aniemeka <ianiemeka@gmail.com>
2018-04-18 14:52:13 -0500

    Display markdown as html using pandoc

--- lib/blog_web/views/post_view.ex
+++ lib/blog_web/views/post_view.ex
@@ -1,10 +1,25 @@
+require IEx
+
 defmodule BlogWeb.PostView do
   use BlogWeb, :view
 
-  def to_markdown(body) do 
-    body
-    |> Earmark.as_html
-    |> elem(1)
+  def to_html(body) do 
+    tmp_dir_path = Path.join(System.tmp_dir, "post_body.md")
+    File.write(tmp_dir_path, body)
+    
+    pandoc_exe = System.find_executable("pandoc")
+    args = [tmp_dir_path, "--from", "markdown", "--to", "html"]
+
+    port = Port.open({:spawn_executable, pandoc_exe}, [:hide, :stderr_to_stdout, :binary, {:args, args}, :exit_status, :use_stdio, :stream])
+
+    body_as_html = receive do 
+      {^port, {:data, html}} -> html
+      _ -> "I'm sorry, Dave."
+    end
+
+    send port, {self(), :close}
+    
+    body_as_html
     |> raw
   end
 end
