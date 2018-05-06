0cf85dee429a88b1d128a2c5e63008453eed1c2b
Ifu Aniemeka <ianiemeka@gmail.com>
2018-05-05 21:45:01 -0500

    Bug: display post descriptions on index page

--- lib/blog_web/views/post_view.ex
+++ lib/blog_web/views/post_view.ex
@@ -8,19 +8,10 @@ defmodule BlogWeb.PostView do
     tmp_dir_path = Path.join(System.tmp_dir, "post_body.md")
     File.write(tmp_dir_path, body)
     
-    pandoc_exe = System.find_executable("pandoc")
     args = [tmp_dir_path, "--from", "markdown", "--to", "html"]
 
-    port = Port.open({:spawn_executable, pandoc_exe}, [:hide, :stderr_to_stdout, :binary, {:args, args}, :exit_status, :use_stdio, :stream])
-
-    body_as_html = receive do 
-      {^port, {:data, html}} -> html
-      _ -> "I'm sorry, Dave."
-    end
-
-    send port, {self(), :close}
-    
-    body_as_html
+    {output, _} = System.cmd "pandoc", args
+    output
   end
 
   def safe_html(body) do 
