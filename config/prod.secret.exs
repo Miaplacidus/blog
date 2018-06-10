4431025f3edb44531304e9143abbac8d96fea69e
Ifu Aniemeka <ianiemeka@gmail.com>
2018-06-10 13:44:38 -0500

    set more env vars

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -9,12 +9,12 @@ use Mix.Config
 # kept out of version control and might be hard to recover
 # or recreate for your teammates (or yourself later on).
 config :blog, BlogWeb.Endpoint,
-  secret_key_base: System.get_env("SECRET_KEY_BASE") 
+  secret_key_base: "${SECRET_KEY_BASE}" 
 
 # Configure your database
 config :blog, Blog.Repo,
   adapter: Ecto.Adapters.Postgres,
-  url: System.get_env("DATABASE_URL"),
+  url: "${DATABASE_URL}",
   database: "",
   pool_size: 1,
   ssl: true
