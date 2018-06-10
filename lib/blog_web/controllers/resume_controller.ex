782df67becc288850130c18a4fed9c48058def07
Ifu Aniemeka <ianiemeka@gmail.com>
2018-06-09 23:27:23 -0500

    Get file from priv/static

--- lib/blog_web/controllers/resume_controller.ex
+++ lib/blog_web/controllers/resume_controller.ex
@@ -2,7 +2,7 @@ defmodule BlogWeb.ResumeController do
   use BlogWeb, :controller
 
   def download_resume(conn, _params) do
-    path = Path.join(System.cwd, "/assets/static/documents/IAniemeka_Resume.pdf")
+    path = Path.join(System.cwd, "/priv/static/documents/IAniemeka_Resume.pdf")
     conn
       |> put_resp_content_type("application/pdf", "utf-8")
       |> send_file(200, path)
