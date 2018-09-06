5ac43bdbdb364c2af2acbe622a57ef77eebe2f0b
Ifu Aniemeka <ianiemeka@gmail.com>
2018-09-06 15:50:49 -0500

    Forgot documents controller

---
+++ lib/blog_web/controllers/documents_controller.ex
@@ -0,0 +1,17 @@
+defmodule BlogWeb.DocumentsController do
+  use BlogWeb, :controller
+
+  def download_resume(conn, _params) do
+    path = Path.join(:code.priv_dir(:blog), "/static/documents/IAniemeka_Resume.pdf")
+    conn
+      |> put_resp_content_type("application/pdf", "utf-8")
+      |> send_file(200, path)
+  end
+
+  def download_physnet_paper(conn, _params) do 
+    path = Path.join(:code.priv_dir(:blog), "/static/documents/intuitive_physics_net_paper.pdf")
+    conn
+      |> put_resp_content_type("application/pdf", "utf-8")
+      |> send_file(200, path)
+  end
+end
