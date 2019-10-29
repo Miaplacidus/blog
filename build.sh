142b411c0e0246d7f63ddfb15ea36d0408b6352b
Ifu Aniemeka <ianiemeka@gmail.com>
2019-10-29 15:31:04 -0500

    install pandoc first

--- build.sh
+++ build.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
+# Install Pandoc
+wget https://github.com/jgm/pandoc/releases/download/2.7.3/pandoc-2.7.3-linux.tar.gz
+tar xvzf pandoc-2.7.3-linux.tar.gz --strip-components 1 -C ~/project/src
+rm pandoc-2.7.3-linux.tar.gz 
+
+# Add Pandoc command
+alias pandoc="~/project/src/bin/pandoc"
+alias pandoc-citeproc="~/project/src/bin/pandoc-citeproc"
+
 # Initial setup
 mix deps.get --only prod
 MIX_ENV=prod mix compile
@@ -10,12 +19,3 @@ mix phx.digest
 
 # Build the release and overwrite the existing release directory
 MIX_ENV=prod mix release --overwrite
-
-# Install Pandoc
-wget https://github.com/jgm/pandoc/releases/download/2.7.3/pandoc-2.7.3-linux.tar.gz
-tar xvzf pandoc-2.7.3-linux.tar.gz --strip-components 1 -C ~/project/src
-rm pandoc-2.7.3-linux.tar.gz 
-
-# Add Pandoc command
-alias pandoc="~/project/src/bin/pandoc"
-alias pandoc-citeproc="~/project/src/bin/pandoc-citeproc"
