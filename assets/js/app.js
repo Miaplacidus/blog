0c4f8ebc7c3025108c9323ce7378099899203750
Ifu Aniemeka <ianiemeka@gmail.com>
2020-06-16 11:25:52 -0500

    get prismjs working again

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,7 @@
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
 import css from "../css/app.scss"
+import Prism from "prismjs"
 
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
